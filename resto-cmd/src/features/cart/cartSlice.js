1ff4b0f86bb4b7b3cfba443c7fcdb9f987b2e483
Gareth Thomas <thomasgareth3@gmail.com>
2024-07-01 23:10:57 +0200

    questions

--- resto-cmd/src/features/cart/cartSlice.js
+++ resto-cmd/src/features/cart/cartSlice.js
@@ -1,23 +1,51 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+//this should work, but doesn't
+// export const cartSlice = createSlice({
+//   name: "list",
+//   initialState: {},
+//   reducers: {
+//     addProduct: (currentState, action) => {
+//       currentState.list.push(action.payload);
+//     },
+//     removeProduct: (currentState, action) => {
+//       currentState.list = currentState.list.filter(
+//         (item, index) => index !== action.payload
+//       );
+//     },
+//     applyVoucher: (currentState, action) => {
+//       currentState.list = currentState.list.map((item) =>
+//         item.title === "Super Crémeux"
+//           ? { ...item, price: action.payload.price }
+//           : item
+//       );
+//     },
+//   },
+// });
+
+// this is with old reducer methods, and works
 export const cartSlice = createSlice({
   name: "list",
   initialState: {},
   reducers: {
     addProduct: (currentState, action) => {
-      currentState.list.push(action.payload);
+      console.log(currentState);
+      const listWithNewProduct = [...currentState, action.payload];
+      return listWithNewProduct;
     },
     removeProduct: (currentState, action) => {
-      currentState.list = currentState.list.filter(
+      const list = [...currentState.list].filter(
         (item, index) => index !== action.payload
       );
+      return list;
     },
     applyVoucher: (currentState, action) => {
-      currentState.list = currentState.list.map((item) =>
+      const withVoucherList = currentState.map((item) =>
         item.title === "Super Crémeux"
           ? { ...item, price: action.payload.price }
           : item
       );
+      return withVoucherList;
     },
   },
 });
